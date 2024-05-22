598355e7150f49f26b9f425e93c66f0364c7d6db
Hannoye Corentin <corentinhnyy@gmail.com>
2024-05-22 19:59:12 +0200

    create composant button for generate apology

--- assets/React/pages/Home/Home.tsx
+++ assets/React/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { APP_TITLE } from "../../../const";
+import ButtonGenerateApology from "./ButtonGenerateApology";
 
 export default function() {
+    const [apology, setApology] = useState(null);
+
     return <>
         <h1 className="mb_40">{ APP_TITLE }</h1>
-        <p>Phrase</p>
-        <button>Générer</button>
+        <p>{ apology ?? "Chargement de l'excuse..." }</p>
+        <ButtonGenerateApology />
     </>;
 }
