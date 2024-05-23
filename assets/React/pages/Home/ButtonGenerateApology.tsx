6acf9657ef5b0483569958e1aebe06172b728a34
Hannoye Corentin <corentinhnyy@gmail.com>
2024-05-23 10:01:27 +0200

    patch bug in ButtongGenerateApology.tsx and update button style

--- assets/React/pages/Home/ButtonGenerateApology.tsx
+++ assets/React/pages/Home/ButtonGenerateApology.tsx
@@ -4,57 +4,61 @@ import { Oval } from "react-loader-spinner";
 
 export default function({ setApology }) {
     const [apologies, setApologies] = useState([]);
-    const [loading, setLoading] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
 
         axios.get("/api/apologies")
         .then(res => {
-            getRandomApology(res.data["hydra:member"]);
+            getRandomApology(res.data["hydra:member"], false);
         });
 
     }, []);
 
-    const getRandomApology = (_apologies: any) => {
-        if(_apologies.length <= 0) {
+    const getRandomApology = (_apologies: any, delay: boolean) => {
+        if(loading || _apologies.length <= 0) {
             return;
         }
 
         // Génération d'un numéro aléatoire pour piocher de façon aléatoirement dans le tableau passé en paramètre
-        const number: number = Math.floor(Math.random() * _apologies.length);
+        const delayNumber: number = delay ? Math.floor(Math.random() * (5 - 1 + 1) + 1) * 1000 : 0;        
 
-        // Passage de l'excuse trouvée au composent parent
-        setApology(_apologies[number].message);
+        setLoading(true);
 
-        // Suppression afin de ne plus retomber dessus pendant cette pioche
-        delete _apologies[number];
+        setTimeout(() => {
+            // Génération d'un numéro aléatoire pour piocher de façon aléatoirement dans le tableau passé en paramètre
+            const number: number = Math.floor(Math.random() * _apologies.length);
 
-        // Filtre du tableau après la suppression pour obtenir un tableau sans objet vide
-        _apologies = _apologies.filter((test: any) => test);
+            // Passage de l'excuse trouvée au composent parent
+            setApology(_apologies[number].message);
 
-        // Mise à jour de la pioche
-        setApologies(_apologies);
+            // Suppression afin de ne plus retomber dessus pendant cette pioche
+            delete _apologies[number];
+
+            // Filtre du tableau après la suppression pour obtenir un tableau sans objet vide
+            _apologies = _apologies.filter((test: any) => test);
+
+            // Mise à jour de la pioche
+            setApologies(_apologies);
+            setLoading(false);
+        }, delayNumber);
     }
 
     // Fonction appelée lors du clique pour générer une nouvelle phrase
     const handleClick = () => {
-        getRandomApology(apologies);
+        getRandomApology(apologies, true);
     }
 
-    return <>
-        <button onClick={handleClick}>
-            {loading ?
-                setApology(<Oval
-                    visible={true}
-                    height="80"
-                    width="80"
-                    color="#4fa94d"
-                    ariaLabel="oval-loading"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                />) :
-                'Générer' 
-            }
-        </button>
-    </>;
+    return <button disabled={loading} onClick={handleClick}>
+        {loading ?
+            <Oval
+                height="20"
+                width="20"
+                color="#4fa94d"
+                ariaLabel="oval-loading"
+                wrapperClass="align_center"
+            /> :
+            'Générer' 
+        }
+    </button>;
 }
